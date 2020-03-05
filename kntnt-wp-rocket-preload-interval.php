b5713effc7bb7871643a4ec3c958550f8fc953c0
Thomas Barregren <thomas@kntnt.com>
2020-03-05 10:59:21 +0100

    Create kntnt-wp-rocket-preload-interval.php

---
+++ kntnt-wp-rocket-preload-interval.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt Preload Interval for WP-Rocket
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Sets the preload interval for WP-Rocket
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+
+add_filter( 'pre_get_rocket_option_sitemap_preload_url_crawl', function () {
+    return 5 * 1000000; // microseconds
+} );
